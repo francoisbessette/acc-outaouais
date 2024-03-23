82d2e08293603c5046fc467b1dea8523ee55086f
francoisbessette <francois.bessette@gmail.com>
2024-03-23 15:58:26 -0400

    Add plugin structure, send new event email, add new users to MailChimp

--- acc-outaouais.php
+++ acc-outaouais.php
@@ -1,70 +1,75 @@
 <?php
-
 /**
  * Plugin Name:       ACC Outaouais
+ * Plugin URI:        https://github.com/francoisbessette/acc-outaouais
  * Description:       Plugin used to customize operation of the ACC Outaouais web site.
- * Version:           1.0
+ * Version:           1.1.0
  * Author:            François Bessette
  * Author URI:        https://github.com/francoisbessette
  * License:           GPL v2 or later
  * Text Domain:       acc-outaouais
+ * Domain Path:       /languages
  */
 
-//if ( ! defined( 'ABSPATH' ) ) {
-//	exit;
-//}
-
-/*
- * Display the organizer email only if user is connected
+/**
+ * This plugin was developed using the WordPress starter plugin template by Arun Basil Lal <arunbasillal@gmail.com>
+ * Please leave this credit and the directory structure intact for future developers who might read the code.
+ * @GitHub https://github.com/arunbasillal/WordPress-Starter-Plugin
  */
-function accou_filter_tribe_get_organizer_email($filtered_email, $unfiltered_email) {
-    error_log("--------------Inside accou_filter_tribe_get_organizer_email--------------");
-    if (is_user_logged_in()) {
-        return($filtered_email);
-    } else {
-        return("");
-    }
+ 
+/**
+ * ~ Directory Structure ~
+ *
+ * /admin/ 					- Plugin backend stuff.
+ * /functions/				- Functions and plugin operations.
+ * /includes/				- External third party classes and libraries.
+ * /languages/				- Translation files go here. 
+ * /public/					- Front end files and functions that matter on the front end go here.
+ * index.php				- Dummy file.
+ * license.txt				- GPL v2
+ * acc-outaouais.php		- Main plugin file containing plugin name and other version info for WordPress.
+ * readme.txt				- Readme for WordPress plugin repository. https://wordpress.org/plugins/files/2018/01/readme.txt
+ * uninstall.php			- Fired when the plugin is uninstalled. 
+ */
+ 
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+/**
+ * Define constants
+ *
+ * @since 1.1
+ */
+if ( ! defined( 'ACCOU_VERSION_NUM' ) ) 		define( 'ACCOU_VERSION_NUM'		, '1.1.0' );
+if ( ! defined( 'ACCOU_PLUGIN' ) )		define( 'ACCOU_PLUGIN'		, trim( dirname( plugin_basename( __FILE__ ) ), '/' ) );
+if ( ! defined( 'ACCOU_PLUGIN_DIR' ) )	define( 'ACCOU_PLUGIN_DIR'	, plugin_dir_path( __FILE__ ) ); // Plugin directory absolute path with the trailing slash. Useful for using with includes eg - /var/www/html/wp-content/plugins/acc-outaouais/
+if ( ! defined( 'ACCOU_PLUGIN_URL' ) )	define( 'ACCOU_PLUGIN_URL'	, plugin_dir_url( __FILE__ ) ); // URL to the plugin folder with the trailing slash. Useful for referencing src eg - http://localhost/wp/wp-content/plugins/acc-outaouais/
+if ( ! defined( 'ACCOU_LOG_DIR' ) )	define( 'ACCOU_LOG_DIR'	, ACCOU_PLUGIN_DIR . 'logs/' );
+
+/**
+ * Database upgrade todo
+ *
+ * @since 1.1
+ */
+function accou_upgrader() {
+	
+	// Get the current version of the plugin stored in the database.
+	$current_ver = get_option( 'accou_version', '0.0' );
+	
+	// Return if we are already on updated version. 
+	if ( version_compare( $current_ver, ACCOU_VERSION_NUM, '==' ) ) {
+		return;
+	}
+	
+	// This part will only be excuted once when a user upgrades from an older version to a newer version.
+	
+	// Finally add the current version to the database. Upgrade todo complete. 
+	update_option( 'accou_version', ACCOU_VERSION_NUM );
 }
+add_action( 'admin_init', 'accou_upgrader' );
 
-/*
- * Display the organizer phone only if user is connected
- */
-function accou_filter_tribe_get_organizer_phone($phone) {
-    error_log("--------------Inside accou_filter_tribe_get_organizer_phone--------------");
-    if (is_user_logged_in()) {
-        return($phone);
-    } else {
-        return("");
-    }
-}
+// Load everything
+require_once( ACCOU_PLUGIN_DIR . 'loader.php' );
 
-
-
-// /**
-//  * Activate the plugin.
-//  */
-// function accou_activate_plugin() {
-//     error_log("--------------Inside accou_activate_plugin--------------");
-
-//     // Customize 'The Event Calendar' to prevent displaying the organizer info to non-connected users
-//     add_filter('tribe_get_organizer_email', 'accou_filter_tribe_get_organizer_email', 10, 2);
-//     add_filter('tribe_get_organizer_phone', 'accou_filter_tribe_get_organizer_phone');
-// }
-
-// /**
-//  * Deactivate the plugin.
-//  */
-// function accou_deactivate_plugin() {
-//     error_log("--------------Inside accou_deactivate_plugin--------------");
-
-//     remove_filter('tribe_get_organizer_email', 'accou_filter_tribe_get_organizer_email', 10);
-//     remove_filter('tribe_get_organizer_phone', 'accou_filter_tribe_get_organizer_phone', 10);
-// }
-
-// register_activation_hook( __FILE__, 'accou_activate_plugin' );
-// register_deactivation_hook( __FILE__, 'accou_deactivate_plugin' );
-
-
-// Customize 'The Event Calendar' to prevent displaying the organizer info to non-connected users
-add_filter('tribe_get_organizer_email', 'accou_filter_tribe_get_organizer_email', 10, 2);
-add_filter('tribe_get_organizer_phone', 'accou_filter_tribe_get_organizer_phone');
+// Register activation hook (this has to be in the main plugin file or refer bit.ly/2qMbn2O)
+register_activation_hook( __FILE__, 'accou_activate_plugin' );
